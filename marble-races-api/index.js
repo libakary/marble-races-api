9d3c4e2018bfe21beaffb1f102481a42768fa42c
Karol <libakary@users.noreply.github.com>
2023-01-19 17:46:35 +0200

    Add dotenv and .env file to store enironment variables [#184267774]

--- marble-races-api/index.js
+++ marble-races-api/index.js
@@ -1,5 +1,6 @@
+require("dotenv").config()
 const app = require("express")()
-const port = 8080
+const port = process.env.APP_PORT
 
 const swaggerUI = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json")
