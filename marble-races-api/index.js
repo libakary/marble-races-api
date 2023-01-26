f3a04cfbf78734c8cc3bcec6e762b3b746a88499
Emma :) <99496596+kodukass@users.noreply.github.com>
2023-01-26 14:06:36 +0200

    show customers?

--- marble-races-api/index.js
+++ marble-races-api/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const app = require("express")()
+const mariadb = require("mariadb")
 const port = process.env.APP_PORT
 
 const swaggerUI = require("swagger-ui-express")
@@ -10,6 +11,31 @@ app.get('/marbles', (req, res) => {
 })
 
 app.use("/docs",swaggerUI.serve,swaggerUI.setup(swaggerDocument))
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 5
+})
+
+app.get("/customers", async(req, res)=>{
+    let connection 
+    try {
+        connection = await pool.getConnection()
+        const rows = await connection.query("SELECT * FROM customers")
+        console.log(rows)
+        res.send(JSON.stringify(rows))
+        
+    } catch (error) {
+        throw error
+    } finally {
+        if (connection) return connection.end()
+    }
+    
+})
+
 
 app.listen(port,()=>{
     console.log(`API up at http://localhost:${port}`)
